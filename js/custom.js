7ecac8aa2b2354c9ad04995b54e1ffb0bea3cfa8
MonCab <monoju_montse15@hotmail.com>
2020-10-03 16:20:29 -0500

    Treatro por la dignidad terminada / Modificacion de Productos

--- js/custom.js
+++ js/custom.js
@@ -212,5 +212,23 @@
 		cursorcolor: "#9b9b9c",
 	});
 	
+	/* ..............................................
+	   Read More
+	   ................................................. */
+
+	$(document).ready(function () {
+    $('.nav-toggle').click(function () {
+        var collapse_content_selector = $(this).attr('href');
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function () {
+            if ($(this).css('display') == 'none') {
+                toggle_switch.html('Read More');
+            } else {
+                toggle_switch.html('Read Less');
+            }
+        });
+    });
+
+});
 	
 }(jQuery));
