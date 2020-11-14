91e4102188b6dfd80c6f67c1444f84fbbe9ba9e3
Tenkoni <caliminion@gmail.com>
2020-11-14 13:37:20 -0600

    Bugfix: La cantidad de items del carrito mostrada por cart-items estaba concatenando strings, no sumando las cantidades, lo que producía inconsistencias en el número de items en el carrito

--- js/app.js
+++ js/app.js
@@ -113,7 +113,7 @@ class UI {
 		let itemsTotal = 0;
 		cart.map(item => {
 			tempTotal += item.price * item.amount;
-			itemsTotal += item.amount;
+			itemsTotal += parseInt(item.amount);
 		});
 		cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
 		cartItems.innerText = itemsTotal;
